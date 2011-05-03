e8fca0151b8d1ab79e7a7c3a6f97d827ede3de26
unknown <Akihito@.(none)>
2011-05-03 17:20:44 +0900

    implements xpath process. get word meaning using xpath expression. todo: create function processing this sequencial steps.

--- get_eijiro.py
+++ get_eijiro.py
@@ -1,14 +1,24 @@
-# -*- codging: utf-8 -*-
+# -*- codging: sjis -*-
 
+import sys
 import httplib
+import lxml.html
+
+word = sys.argv[1]
 
 h = httplib.HTTPConnection("eow.alc.co.jp")
 #maybe, eijiro filter user-agent by python httplib. so, change user-agent.
-h.request("GET", "/sample/UTF-8", None, {"User-Agent":"Mozilla"})
+h.request("GET", "/%s/UTF-8" % word, None, {"User-Agent":"Mozilla"})
 r = h.getresponse()
 if r.status == httplib.OK:
 	data = r.read()
-	print data
-print r.read()
-
-
+	#print data
+	xRoot = lxml.html.fromstring(data)
+	wordclass = '//div[@id="resultsList"]//li[1]//span[@class="wordclass"]/text()'
+	meaning = '//div[@id="resultsList"]//li[1]//ol/li/text()'
+	#meaning = '//div[@id="resultsList"]//li[1]//ol[%s]/li[1]/text()' % str(1)
+	d = xRoot.xpath(wordclass)
+	print "-----" + word + "-----"
+	print "".join(d)
+	d = xRoot.xpath(meaning)
+	print "".join(d)
